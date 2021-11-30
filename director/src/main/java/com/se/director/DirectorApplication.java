0da204cf078afbf198c537d77d014cf0d61ec02e
Nhat Hung <hungnguyen.130420@gmail.com>
2021-11-30 12:03:17 +0700

    discovery,config

--- director/src/main/java/com/se/director/DirectorApplication.java
+++ director/src/main/java/com/se/director/DirectorApplication.java
@@ -2,8 +2,10 @@ package com.se.director;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DirectorApplication {
 
 	public static void main(String[] args) {
