cd2e1c0304ce1d8359e4bab4ddef61838c759ada
NguyenThanhHoai15 <thanhhoai150420@gmail.com>
2021-12-05 15:06:18 +0700

    Hoai commit catche redis

--- director/src/main/java/com/se/director/service/DirectorService.java
+++ director/src/main/java/com/se/director/service/DirectorService.java
@@ -4,8 +4,12 @@ import com.se.director.model.Director;
 import com.se.director.repository.DirectorRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,11 +18,36 @@ public class DirectorService {
     @Autowired
     private DirectorRepository directorRepository;
 
+
+    @Cacheable(value = "Director", key = "#id")
     public Director getDirectorById(Long id){
         Optional<Director> obj = directorRepository.findById(id);
         if(obj.isPresent()){
             return obj.get();
         }
         return null;
+    }
+
+    @Cacheable(value = "Director")
+    public List<Director>getAllDirector(){
+        return directorRepository.findAll();
+    }
+
+    @CachePut(value = "Director", key = "#id")
+    public Director updateDirector(Director director, Long id){
+        Director dir = directorRepository.findById(id).get();
+        dir.setFirstName(director.getFirstName());
+        dir.setLastName(director.getLastName());
+        dir.setAge(director.getAge());
+        dir.setAddress(director.getAddress());
+        dir.setGender(director.isGender());
+        dir.setUsername(director.getUsername());
+        dir.setPassword(director.getPassword());
+        return directorRepository.save(dir);
+    }
+
+    @CacheEvict(value = "Director", key = "#id")
+    public void deleteDirector(Long id){
+        directorRepository.deleteById(id);
     }
 }
