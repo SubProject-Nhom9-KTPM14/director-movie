c550298d532e11f1cd1d0584bd85f284db673c25
Nam Bui <buithanhnam26092000@gmail.com>
2021-12-06 10:22:24 +0700

    ratelimiter

--- movie/src/main/java/com/se/movie/model/SearchRequest.java
+++ movie/src/main/java/com/se/movie/model/SearchRequest.java
@@ -1,2 +1,25 @@
-package com.se.movie.model;public class SearchRequest {
+package com.se.movie.model;
+
+public class SearchRequest {
+    String from;
+    String to;
+    String flightDate;
+
+    public SearchRequest(String from, String to, String flightDate) {
+        this.from = from;
+        this.to = to;
+        this.flightDate = flightDate;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getFlightDate() {
+        return flightDate;
+    }
 }
--- movie/src/main/java/com/se/movie/service/MovieService.java
+++ movie/src/main/java/com/se/movie/service/MovieService.java
@@ -2,14 +2,21 @@ package com.se.movie.service;
 
 import com.se.movie.VO.Director;
 import com.se.movie.VO.ResponseTemplateVO;
+import com.se.movie.model.Attendance;
 import com.se.movie.model.Movie;
+import com.se.movie.model.SearchRequest;
 import com.se.movie.repository.MovieRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import io.github.resilience4j.retry.annotation.Retry;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,7 +35,7 @@ public class MovieService {
         }
         return null;
     }
-
+    @Retry(name="basic")
     @Cacheable(value = "MovieApplication", key = "movieId")
     public ResponseTemplateVO getMovieWithDirectorById(Long movieId) {
         ResponseTemplateVO vo = new ResponseTemplateVO();
@@ -42,5 +49,27 @@ public class MovieService {
         vo.setDirector(director);
 
         return  vo;
+    }
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss SSS");
+
+    public List<Movie> searchFlights(SearchRequest request) {
+        System.out.println("Searching for flights; "
+                + "current time = " + LocalDateTime.now().format(formatter) +
+                "; current thread = " + Thread.currentThread().getName());
+
+        List<Movie> flights = Arrays.asList(
+//                new Flight("XY 765", request.getFlightDate(), request.getFrom(), request.getTo()),
+//                new Flight("XY 746", request.getFlightDate(), request.getFrom(), request.getTo())
+        );
+        System.out.println("Flight search successful");
+        return flights;
+    }
+
+    public List<Movie> searchFlightsThrowingException(SearchRequest request) throws Exception {
+        System.out.println("Searching for flights; "
+                + "current time = " + LocalDateTime.now().format(formatter) +
+                "; current thread = " + Thread.currentThread().getName());
+
+        throw new Exception("Exception when searching for flights");
     }
 }
