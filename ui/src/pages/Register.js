9074c82d7c1f1a819902237ae42af4708a4e2de9
Le Nhat Huy <lenhathuy2212000@gmail.com>
2021-12-08 18:41:57 +0700

    deploy eureka, cloud-config

--- ui/src/pages/Register.js
+++ ui/src/pages/Register.js
@@ -1,12 +1,11 @@
-
-import Formsy from 'formsy-react';
+import Formsy from "formsy-react";
 import useForm from "./Form/useForm";
 import classes from "./Regsiter.module.css";
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import * as actions from '../actions/director.action';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import * as actions from "../actions/director.action";
 import { useNavigate } from "react-router-dom";
-import { useAlert } from 'react-alert'
+import { useAlert } from "react-alert";
 import {
   Typography,
   Button,
@@ -17,32 +16,30 @@ import {
   FormLabel,
 } from "@material-ui/core";
 const initialFieldValues = {
-  address: '',
-  password: '',
-  lastName: '',
-  firstName: '',
-  username: '',
-  age: '',
+  address: "",
+  password: "",
+  lastName: "",
+  firstName: "",
+  username: "",
+  age: "",
   gender: true,
-  passwordconfirm: ''
-}
+  passwordconfirm: "",
+};
 const Register = ({ ...props }) => {
   const [checked, setChecked] = useState({ nam: true, nu: false });
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  let allDirect = useSelector(state => state.director.allDirector);
-  const [user, setUser] = useState([])
-  const alert = useAlert()
+  let allDirect = useSelector((state) => state.director.allDirector);
+  const [user, setUser] = useState([]);
+  const alert = useAlert();
   //Get All Director
   useEffect(() => {
-    dispatch(actions.getAllDirec())
+    dispatch(actions.getAllDirec());
   }, []);
-  console.log(allDirect)
+  console.log(allDirect);
   useEffect(() => {
-    if (allDirect) (
-      setUser(allDirect)
-    )
-  }, [allDirect])
+    if (allDirect) setUser(allDirect);
+  }, [allDirect]);
 
   const onChange = (event) => {
     if (event.target.name === "nam") {
@@ -51,91 +48,107 @@ const Register = ({ ...props }) => {
       setChecked({ nam: false, nu: true });
     }
   };
-  console.log(user)
+  console.log(user);
   //Validate
   const validate = (fieldValues = values) => {
-    let temp = { ...errors }
-    if ('username' in fieldValues) {
+    let temp = { ...errors };
+    if ("username" in fieldValues) {
       let err = 0;
       user.map((user) => {
-        if (user.username.toLowerCase() === fieldValues.username.toLowerCase()) {
-          err = err + 1
+        if (
+          user.username.toLowerCase() === fieldValues.username.toLowerCase()
+        ) {
+          err = err + 1;
         }
-      })
-      if (fieldValues.username === '') {
-        temp.username = fieldValues.username ? "" : "Username is required."
-      } if (fieldValues.username !== '') {
-        temp.username = (/^[A-Za-z1-9]\w{5,14}$/).test(fieldValues.username) ? "" : "Username must be at least 6 and at most 15 characters without any special characters"
+      });
+      if (fieldValues.username === "") {
+        temp.username = fieldValues.username ? "" : "Username is required.";
+      }
+      if (fieldValues.username !== "") {
+        temp.username = /^[A-Za-z1-9]\w{5,14}$/.test(fieldValues.username)
+          ? ""
+          : "Username must be at least 6 and at most 15 characters without any special characters";
       }
       if (err >= 1) {
-        err < 1 ? temp.username = "" : temp.username = "Username is existed"
+        err < 1
+          ? (temp.username = "")
+          : (temp.username = "Username is existed");
       }
     }
-    if ('lastName' in fieldValues) {
-      if (fieldValues.lastName === '') {
-        temp.lastName = fieldValues.lastName ? "" : "lastName is required."
-      } if (fieldValues.lastName !== '') {
-        temp.lastName = (/^[a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]{1,15}(?: [a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]+){0,6}$/).test(fieldValues.lastName) ? "" : "lastName is not valid."
+    if ("lastName" in fieldValues) {
+      if (fieldValues.lastName === "") {
+        temp.lastName = fieldValues.lastName ? "" : "lastName is required.";
+      }
+      if (fieldValues.lastName !== "") {
+        temp.lastName =
+          /^[a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]{1,15}(?: [a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]+){0,6}$/.test(
+            fieldValues.lastName
+          )
+            ? ""
+            : "lastName is not valid.";
       }
     }
-    if ('firstName' in fieldValues) {
-      if (fieldValues.firstName === '') {
-        temp.firstName = fieldValues.firstName ? "" : "First is required."
-      } if (fieldValues.firstName !== '') {
-        temp.firstName = (/^[a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]{1,15}(?: [a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]+){0,6}$/).test(fieldValues.firstName) ? "" : "firstName is not valid."
+    if ("firstName" in fieldValues) {
+      if (fieldValues.firstName === "") {
+        temp.firstName = fieldValues.firstName ? "" : "First is required.";
+      }
+      if (fieldValues.firstName !== "") {
+        temp.firstName =
+          /^[a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]{1,15}(?: [a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]+){0,6}$/.test(
+            fieldValues.firstName
+          )
+            ? ""
+            : "firstName is not valid.";
       }
     }
-    if ('address' in fieldValues) {
-      temp.address = fieldValues.address ? "" : "First is required."
+    if ("address" in fieldValues) {
+      temp.address = fieldValues.address ? "" : "First is required.";
     }
-    if ('age' in fieldValues) {
-      temp.age = fieldValues.age ? "" : "Age is required"
+    if ("age" in fieldValues) {
+      temp.age = fieldValues.age ? "" : "Age is required";
     }
-    if ('password' in fieldValues)
-      if (fieldValues.password === '') {
-        temp.password = fieldValues.password ? "" : "Password is required."
-      } if (fieldValues.password !== '') {
-        temp.password = (/^[A-Za-z0-9!@#$%^&*]{6,20}$/).test(fieldValues.password) ? "" : "Password must be at least 6 and at most 15 characters"
+    if ("password" in fieldValues)
+      if (fieldValues.password === "") {
+        temp.password = fieldValues.password ? "" : "Password is required.";
       }
-    if ('passwordconfirm' in fieldValues) {
-      fieldValues.passwordconfirm == values.password ? temp.passwordconfirm = "" : temp.passwordconfirm = "Confirmation password does not match"
+    if (fieldValues.password !== "") {
+      temp.password = /^[A-Za-z0-9!@#$%^&*]{6,20}$/.test(fieldValues.password)
+        ? ""
+        : "Password must be at least 6 and at most 15 characters";
+    }
+    if ("passwordconfirm" in fieldValues) {
+      fieldValues.passwordconfirm == values.password
+        ? (temp.passwordconfirm = "")
+        : (temp.passwordconfirm = "Confirmation password does not match");
     }
 
     setErrors({
-      ...temp
-    })
+      ...temp,
+    });
 
-    if (fieldValues == values)
-      return Object.values(temp).every(x => x == "")
-  }
+    if (fieldValues == values) return Object.values(temp).every((x) => x == "");
+  };
   const Title = (
     <Typography variant="h5" className={classes.title}>
       REGISTER
     </Typography>
   );
 
-  const {
-    values,
-    setValues,
-    errors,
-    setErrors,
-    handleInputChange,
-    resetForm
-  } = useForm(initialFieldValues, validate, props.setCurrentId)
-  console.log(values)
-  const handleSubmit = e => {
-    if (validate()) {
-      dispatch(actions.register(values))
-      alert.success('Register Successfull !!! Login now !')
-      navigate("/login")
-    }
+  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
+    useForm(initialFieldValues, validate, props.setCurrentId);
 
-  }
+  console.log(values);
+
+  const handleSubmit = (e) => {
+    if (validate()) {
+      dispatch(actions.register(values));
+      alert.success("Register Successfull !!! Login now !");
+      navigate("/login");
+    }
+  };
   return (
     <main className={classes.container}>
-      <Formsy className={classes.form}
-        onSubmit={handleSubmit}
-      >
+      <Formsy className={classes.form} onSubmit={handleSubmit}>
         {Title}
         <TextField
           id="first-name"
@@ -145,7 +158,10 @@ const Register = ({ ...props }) => {
           helperText=" "
           value={values.firstName}
           onChange={handleInputChange}
-          {...(errors.firstName && { error: true, helperText: errors.firstName })}
+          {...(errors.firstName && {
+            error: true,
+            helperText: errors.firstName,
+          })}
         />
         <TextField
           id="last-name"
@@ -166,8 +182,9 @@ const Register = ({ ...props }) => {
           type="number"
           InputProps={{
             inputProps: {
-              max: 100, min: 18
-            }
+              max: 100,
+              min: 18,
+            },
           }}
           value={values.age}
           onChange={handleInputChange}
@@ -218,7 +235,6 @@ const Register = ({ ...props }) => {
           helperText=" "
           name="username"
           value={values.username}
-
           onChange={handleInputChange}
           {...(errors.username && { error: true, helperText: errors.username })}
         />
@@ -242,7 +258,10 @@ const Register = ({ ...props }) => {
           type="password"
           value={values.passwordconfirm}
           onChange={handleInputChange}
-          {...(errors.passwordconfirm && { error: true, helperText: errors.passwordconfirm })}
+          {...(errors.passwordconfirm && {
+            error: true,
+            helperText: errors.passwordconfirm,
+          })}
         />
 
         <Button
